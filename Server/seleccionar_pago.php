2054d5855ce4e77bec2a284d243d4b4d1830dc17
MarcosDan98 <md.lara16@info.uas.edu.mx>
2021-01-08 23:16:01 -0700

    v.2.3

--- Server/seleccionar_pago.php
+++ Server/seleccionar_pago.php
@@ -3,20 +3,20 @@
 include('Conexion.php');
 session_start();
 
-if (isset($_POST['verificar'])) {
+if (isset($_POST['seleccionar'])) {
 
-    $Num_tajeta= $_POST['Num_tajeta'];
+    $Num_tarjeta= $_POST['Num_tarjeta'];
     $ID = $_POST['ID'];
     $id_h= $_POST['id_h'];
     
  
-    $sql= "UPDATE tarjeta_bancaria set Preferido= 'Si' where Num_tajeta = '$Num_tajeta'";
+    $sql= "UPDATE tarjeta_bancaria set Preferido = 'Si' where Num_tarjeta = '$Num_tarjeta'";
     if ($conn->query($sql)===TRUE) {
-        header("Location:../banking_services/select_pay_method.php?id_h=$id_h");
+        header("Location:../myaccount/summary.php?id_h=$id_h");
     }
     else{
         $_SESSION['error']="Ha ocurrido un error al registrar su cuenta, intentelo de nuevo.";
-        header('Location:../banking_services/add_card.php?id_h=$id_h');
+        header('Location:../banking_services/select_pay_method.php?id_h=$id_h');
     }
 
 }
