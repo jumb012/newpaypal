5064bbd2599a7142f1a931ce6c62f5ff7f65688b
jumb012 <jassiel_martinez@hotmail.com>
2021-01-09 13:16:10 -0700

    v2.5.1

--- components/copy.php
+++ components/copy.php
@@ -6,5 +6,5 @@ if(isset($_SESSION['Summary'])){
 }
 ?>
 <div class="container-fluid bg-light py-2">
-  <p class="text-center text-1 text-muted mb-0">Copyright © 2020 <a href="<?=$mensaje?>">PayPal</a>. All Rights Reserved.</p>
+  <p class="text-center text-1 text-muted mb-0">Copyright © <?php echo date("Y")?> <a href="<?=$mensaje?>">PayPal</a>. All Rights Reserved.</p>
 </div>
