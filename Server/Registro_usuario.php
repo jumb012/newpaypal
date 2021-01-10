8648a08f96e7ec84baaf8a30a61f8a22dc15794a
Carlos Duarte <kiniorap1998@gmail.com>
2021-01-09 18:29:55 -0700

    V2.6

--- Server/Registro_usuario.php
+++ Server/Registro_usuario.php
@@ -1,32 +1,51 @@
 <?php
 include('Conexion.php');
-session_start();
-if (isset($_POST['txtTelefono'])) {
-    $Num_telefono = $_POST['txtTelefono'];
+
+if(isset($_POST['txtEmail'])){
     $Correo = $_POST['txtEmail'];
     $Nombre = $_POST['txtNombre'];
-    $Ap_Pat = $_POST['txtApellidoP'];
-    $Ap_Mat = $_POST['txtApellidoM'];
     $Contraseña = $_POST['txtPassword'];
     $Vcontraseña = $_POST['txtVpassword'];
-    $Fecha_nacimiento = $_POST['dateFechaNac'];
+    $Contraseña_h = md5($Contraseña);
     $Direccion = $_POST['txtDireccion'];
     $Colonia = $_POST['txtColonia'];
     $Codigo_postal = $_POST['txtCodPostal'];
     $Ciudad = $_POST['txtCiudad'];
     $Estado = $_POST['selEstados'];
-    $Tipo_Cuenta = $_POST['txtTipoCuenta'];
-    $id_h = md5($Num_telefono);
-
-
-
-    $sql= "INSERT INTO registro_usuario(Num_telefono,Correo,Nombre,Ap_pat,Ap_mat,Contraseña,Fecha_nacimiento,Direccion,Colonia,Codigo_postal,Ciudad,Estado,Tipo_Cuenta,id_h) 
-    VALUES ('$Num_telefono','$Correo','$Nombre','$Ap_Pat','$Ap_Mat','$Contraseña','$Fecha_nacimiento','$Direccion','$Colonia','$Codigo_postal','$Ciudad','$Estado','$Tipo_Cuenta','$id_h')";
-    if ($conn->query($sql)===TRUE) {
-        header("Location:../banking_services/bank_account.php?id_h=$id_h");
+    $id_h = md5($Correo);     
+    if($_POST['txtTipoCuenta']==1){
+        $TipoCuenta="Personal";
+        $Num_telefono = $_POST['txtTelefono'];
+        $Ap_Pat = $_POST['txtApellidoP'];
+        $Ap_Mat = $_POST['txtApellidoM'];
+        $Fecha_nacimiento = $_POST['dateFechaNac'];        
+        $sql= "INSERT INTO registro_usuario(Num_telefono,Correo,Nombre,Ap_pat,Ap_mat,Contraseña,Fecha_nacimiento,Direccion,Colonia,Codigo_postal,Ciudad,Estado,Tipo_Cuenta,id_h) 
+        VALUES ('$Num_telefono','$Correo','$Nombre','$Ap_Pat','$Ap_Mat','$Contraseña_h','$Fecha_nacimiento','$Direccion','$Colonia','$Codigo_postal','$Ciudad','$Estado','$TipoCuenta','$id_h')";    
+        if ($conn->query($sql)===TRUE) {
+            header("Location:../signin.php");
+        }else{
+            $_SESSION['error']="Ha ocurrido un error al registrar su cuenta, intentelo de nuevo.";
+            header('Location:../welcome/signup_personal.php');
+        }
     }else{
-        $_SESSION['error']="Ha ocurrido un error al registrar su cuenta, intentelo de nuevo.";
-        header('Location:../welcome/signup_personal.php');
+        $TipoCuenta="Empresarial";
+        $RFC = $_POST['txtRFC'];
+        $MSI = $_POST['selMSI'];
+        if ($MSI==TRUE) {
+            $MSIR = '1';
+        }else {
+            $MSIR = '0';
+        }
+        $VentasMensuales = $_POST['selVentas'];
+        $sql= "INSERT INTO registro_usuario(Correo,Nombre,Contraseña,Direccion,Colonia,Codigo_postal,Ciudad,Estado,Tipo_Cuenta,id_h,Ventas_Mensuales,MSI,RFC) 
+        VALUES ('$Correo','$Nombre','$Contraseña_h','$Direccion','$Colonia','$Codigo_postal','$Ciudad','$Estado','$TipoCuenta','$id_h','$VentasMensuales','$MSIR','$RFC')";    
+        if ($conn->query($sql)===TRUE) {
+            header("Location:../signin.php");
+        }else{
+            $_SESSION['error']="Ha ocurrido un error al registrar su cuenta, intentelo de nuevo.";
+           header('Location:../welcome/signup_personal.php');
+        }
     }
-} 
+    
+}
 ?>
